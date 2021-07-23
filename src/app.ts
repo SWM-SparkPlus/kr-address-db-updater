023538938712afef0c9ec41d534b69f02c473eb2
taypark <interrupt@kakao.com>
2021-07-23 20:04:18 +0900

    Complete: Daily download-extract-utf8conversion script

--- src/app.ts
+++ src/app.ts
@@ -7,10 +7,10 @@ import iconv from 'iconv-lite'
 import { logger } from './lib/logger'
 
 // 어제 날짜 구하기
-const yesterday = dayjs(new Date().setDate(new Date().getDate())).format('YYYYMMDD')
+const yesterday = dayjs(new Date().setDate(new Date().getDate() - 2)).format('YYYYMMDD')
 
 // 요청 주소(어제자 일일 변경분)
-const url = `https://www.juso.go.kr/dn.do?reqType=DC&stdde=${yesterday}&indutyCd=999&purpsCd=999&indutyRm=수집종료&purpsRm=수집종료`
+const url = encodeURI(`https://www.juso.go.kr/dn.do?reqType=DC&stdde=${yesterday}&indutyCd=999&purpsCd=999&indutyRm=수집종료&purpsRm=수집종료`)
 
 const rootDir = path.resolve(__dirname + '/..')
 const resourcePath = `${rootDir}/resources`
@@ -19,7 +19,7 @@ const resourcePath = `${rootDir}/resources`
 if (fs.existsSync(resourcePath)) {
   try {
     fs.rmSync(resourcePath, { recursive: true, force: true })
-    logger.info(`[DailyUpdatePreparation] Resource directory deletion completed`)
+    logger.info(`[DailyUpdatePreparation] Resource directory deletion completed.`)
   } catch (err) {
     logger.error(`[DailyUpdatePreparation] ${err}`)
     process.exit(1)
@@ -29,7 +29,7 @@ if (fs.existsSync(resourcePath)) {
 // 디렉토리 생성
 try {
   fs.mkdirSync(resourcePath)
-  logger.info(`[DailyUpdatePreparation] Make directory successfully`)
+  logger.info(`[DailyUpdatePreparation] Make directory successfully.`)
 } catch (err) {
   logger.error(`[DailyUpdatePreparation] ${err}`)
   process.exit(1)
@@ -43,9 +43,8 @@ const writeStream = fs.createWriteStream(zipPath)
 // 변경분 다운로드
 https.get(url, res => {
   if (res.statusCode)
-
-  // 다운로드 데이터를 쓰기스트림에 파이프라이닝
-  res.pipe(writeStream)
+    // 다운로드 데이터를 쓰기스트림에 파이프라이닝
+    res.pipe(writeStream)
 
   // 스트림 입력 종료시에 압축 해제
   writeStream.on('finish', async () => {
@@ -76,9 +75,15 @@ https.get(url, res => {
       fs.writeFile(`${resourcePath}/${fileName}`, utf8EncodedContent, err => {
         if (err) {
           logger.error(`[UTF8FileOverwriteError] ${err.message} ${err.stack}`)
+          process.exit(1)
         }
       })
       logger.info(`[UTF8FileOverWrite] Successfully convert ${fileName} encoding EUC-KR to UTF8.`)
     })
+    
+    logger.info(
+      `[DailyDownloadCompletion] Daily roadname file download, extract, conversion completed.`
+    )
   })
 })
+
