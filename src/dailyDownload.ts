c21134e98693cbfb4a857c8bb24bc2d2480bfd93
Tay Park <interrupt@kakao.com>
2021-08-02 08:29:33 +0900

    Refactor daily download to remove dependencies

--- src/dailyDownload.ts
+++ src/dailyDownload.ts
@@ -1,10 +1,9 @@
 import dayjs from 'dayjs'
-import https from 'https'
 import fs from 'fs'
 import path from 'path'
 import iconv from 'iconv-lite'
-import Zip from 'adm-zip'
 import { logger } from './lib/logger'
+import { downloadFile, TDownloadFileOption } from './lib/fileDownloader'
 
 // 어제 날짜 구하기
 const date = new Date()
@@ -16,12 +15,12 @@ const url = encodeURI(
 )
 
 const rootDir = path.resolve(__dirname + '/..')
-const targetPath = `${rootDir}/resources/daily`
+const downloadDir = `${rootDir}/resources/daily`
 
 // 클린 다운로드를 위해 기존 리소스 삭제
-if (fs.existsSync(targetPath)) {
+if (fs.existsSync(downloadDir)) {
   try {
-    fs.rmSync(targetPath, { recursive: true, force: true })
+    fs.rmSync(downloadDir, { recursive: true, force: true })
     logger.info(`[DailyUpdatePreparation] Resource directory deletion completed.`)
   } catch (err) {
     logger.error(`[DailyUpdatePreparation] ${err}`)
@@ -31,7 +30,7 @@ if (fs.existsSync(targetPath)) {
 
 // 디렉토리 생성
 try {
-  fs.mkdirSync(targetPath, { recursive: true })
+  fs.mkdirSync(downloadDir, { recursive: true })
   logger.info(`[DailyUpdatePreparation] Make directory successfully.`)
 } catch (err) {
   logger.error(`[DailyUpdatePreparation] ${err}`)
@@ -40,53 +39,30 @@ try {
 
 // 파일이름, 디렉토리명, WriteStream 생성
 const fileName = `${yesterday}_update.zip`
-const zipPath = `${targetPath}/${fileName}`
-const writeStream = fs.createWriteStream(zipPath)
+const zipPath = `${downloadDir}/${fileName}`
+const downloadStream = fs.createWriteStream(zipPath)
+const downloadOption: TDownloadFileOption = {
+  url,
+  writeStream: downloadStream,
+  downloadDir,
+}
 
-// 변경분 다운로드
-https.get(url, res => {
-  if (res.statusCode)
-    // 다운로드 데이터를 쓰기스트림에 파이프라이닝
-    res.pipe(writeStream)
+const main = async () => {
+  ;(await downloadFile(downloadOption)).forEach(entry => {
+    entry.getDataAsync((data, err) => {
+      if (err) throw err
 
-  // 스트림 입력 종료시에 압축 해제
-  writeStream.on('finish', async () => {
-    try {
-      new Zip(zipPath).getEntries().forEach(entry => {})
-    } catch (err) {
-      // zip 파일이 아니거나, 온전하지 못하거나, 날짜에 맞는 파일을 다운받지 못했을 경우 에러 발생
-      logger.error(`[ZipExtractError] ${err}`)
-      fs.rmSync(targetPath, { force: true, recursive: true })
-      process.exit(1)
-    }
+      const encodedContent = iconv.decode(data, 'euc-kr')
 
-    // 압축해제 후 삭제
-    try {
-      fs.rmSync(zipPath)
-    } catch (err) {
-      logger.error(`[ZipFileRemoveError] ${err}`)
-      process.exit(1)
-    }
-
-    // 파일리스트
-    const eucKrFiles = fs.readdirSync(targetPath)
-
-    eucKrFiles.forEach(fileName => {
-      const euckrContent = fs.readFileSync(`${targetPath}/${fileName}`)
-      // euc-kr 로 decode
-      const utf8EncodedContent = iconv.decode(euckrContent, 'euc-kr')
-      // utf8로 async overwrite
-      fs.writeFile(`${targetPath}/${fileName}`, utf8EncodedContent, err => {
-        if (err) {
-          logger.error(`[UTF8ConversionError] ${err.message} ${err.stack}`)
-          process.exit(1)
-        }
+      fs.writeFile(`${downloadDir}/${entry.entryName}`, encodedContent, err => {
+        if (err) throw err
       })
-      logger.info(
-        `[UTF8ConversionCompletion] Successfully convert ${fileName} encoding EUC-KR to UTF8.`
-      )
     })
-
-    logger.info(`[DailyDownloadCompletion] Job finished!`)
   })
-})
+}
+
+main()
+  .then(() => logger.info(`[DailyDownloadComplete] Job finished.`))
+  .catch(err => {
+    logger.error(`[DailyDownloadError] ${err}`)
+  })
