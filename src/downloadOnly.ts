3a93fa6af2b2acabc765acbeeaeb4bee8f83e358
Tay Park <interrupt@kakao.com>
2021-08-08 22:59:57 +0900

    Complete download only

--- src/downloadOnly.ts
+++ src/downloadOnly.ts
@@ -17,59 +17,60 @@ downloadOnlyEvent.on('finish', (target: string) => {
 const argCandidate = ['--daily', '-d', '--total', '-t']
 const date = new Date()
 
-try {
-  ;async () => {
-    if (argCandidate.includes(target)) {
-      let url: string
-      let downloadDir: string
-      let writeStream = createWriteStream(downloadDir)
+async function main() {
+  if (argCandidate.includes(target)) {
+    let url = ''
+    let downloadDir = ''
 
-      // 매개변수에 따라 다르게 처리
-      if (target === '--daily' || target === '-d') {
-        logger.info(`[DownloadDailyStart]`)
+    // 매개변수에 따라 다르게 처리
+    if (target === '--daily' || target === '-d') {
+      logger.info(`[DownloadDailyStart]`)
 
-        const yesterday = dayjs(date.setDate(date.getDate() - 1)).format('YYYYMMDD')
-        url = encodeURI(
-          `https://www.juso.go.kr/dn.do?reqType=DC&stdde=${yesterday}&indutyCd=999&purpsCd=999&indutyRm=수집종료&purpsRm=수집종료`
-        )
-        downloadDir = dailyDir
-      } else if (target === '--total' || target === '-t') {
-        logger.info(`[DownloadTotalStart]`)
-
-        const previousMonth = dayjs(date.setMonth(date.getMonth() - 2)).format('YYYYMM')
-        url = encodeURI(
-          `https://www.juso.go.kr/dn.do?reqType=ALLMTCHG&regYmd=${previousMonth.slice(
-            0,
-            4
-          )}&ctprvnCd=00&gubun=MTCH&stdde=${previousMonth}&fileName=${previousMonth}_주소DB_전체분.zip&realFileName=${previousMonth}ALLMTCHG00.zip&indutyCd=999&purpsCd=999&indutyRm=수집종료&purpsRm=수집종료`
-        )
-        downloadDir = totalDir
-      }
-
-      if (!existsSync(downloadDir)) {
-        mkdirSync(downloadDir, { recursive: true })
-      }
-
-      ;(await downloadFileAndGetEntries({ url, writeStream } as TDownloadFileOption)).forEach(
-        entry => {
-          entry.getDataAsync((data, err) => {
-            if (err) throw err
-
-            writeEncodedFileAndImport({
-              data,
-              entryOfZip: entry,
-              writeDir: downloadDir,
-              doImport: false,
-            })
-          })
-        }
+      const yesterday = dayjs(date.setDate(date.getDate() - 1)).format('YYYYMMDD')
+      url = encodeURI(
+        `https://www.juso.go.kr/dn.do?reqType=DC&stdde=${yesterday}&indutyCd=999&purpsCd=999&indutyRm=수집종료&purpsRm=수집종료`
       )
-    } else {
-      logger.error(`[UnexpectedArgvError] argv ${target} is unsupported.`)
-      process.exit(0)
+      downloadDir = dailyDir
+    } else if (target === '--total' || target === '-t') {
+      logger.info(`[DownloadTotalStart]`)
+
+      const previousMonth = dayjs(date.setMonth(date.getMonth() - 2)).format('YYYYMM')
+      url = encodeURI(
+        `https://www.juso.go.kr/dn.do?reqType=ALLMTCHG&regYmd=${previousMonth.slice(
+          0,
+          4
+        )}&ctprvnCd=00&gubun=MTCH&stdde=${previousMonth}&fileName=${previousMonth}_주소DB_전체분.zip&realFileName=${previousMonth}ALLMTCHG00.zip&indutyCd=999&purpsCd=999&indutyRm=수집종료&purpsRm=수집종료`
+      )
+      downloadDir = totalDir
     }
+
+    if (!existsSync(downloadDir)) {
+      mkdirSync(downloadDir, { recursive: true })
+    }
+
+    const writeStream = createWriteStream(`${downloadDir}/address_file.zip`)
+    ;(await downloadFileAndGetEntries({ url, writeStream } as TDownloadFileOption)).forEach(
+      entry => {
+        entry.getDataAsync((data, err) => {
+          if (err) throw err
+
+          writeEncodedFileAndImport({
+            data,
+            entryOfZip: entry,
+            writeDir: downloadDir,
+            doImport: false,
+          })
+        })
+      }
+    )
+  } else {
+    logger.error(`[UnexpectedArgvError] argv ${target} is unsupported.`)
+    process.exit(0)
   }
-} catch (err) {
-  logger.error(`[UnexpectedError] ${err}`)
-  process.exit(1)
 }
+
+main()
+  .then()
+  .catch(e => {
+    logger.error(e)
+  })
