f2d1ed455e58ba6c9db83ee3d13cc40015cc5431
Tay Park <interrupt@kakao.com>
2021-08-15 09:11:16 +0900

    Edit schema as length of table_name

--- scripts/create_index.sh
+++ scripts/create_index.sh
@@ -11,11 +11,11 @@ reference_table=$2
 
 create_index_script="
 INSERT INTO $target (manage_number, tablename)
-SELECT manage_number, (SELECT TABLE_NAME
+  SELECT manage_number, (SELECT TABLE_NAME
                       FROM INFORMATION_SCHEMA.STATISTICS
                       WHERE TABLE_SCHEMA='$MYSQL_DATABASE'
                       AND TABLE_NAME='$reference_table')
-FROM $reference_table
+  FROM $reference_table
 "
 
 docker exec $(docker ps | grep mysql | awk '{ print $1 }') mysql -h$MYSQL_HOST -P$MYSQL_PORT -uroot -p$MYSQL_ROOT_PASSWORD -D$MYSQL_DATABASE -e "$create_index_script" >> ./logs/info/mysql_create_index.log
