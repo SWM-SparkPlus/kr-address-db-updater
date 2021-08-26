b616102a4bc42249429cfd9be5cdb229ee53a1fa
Tay Park <interrupt@kakao.com>
2021-08-26 13:03:58 +0900

    Add typeorm config and variables on .env

---
+++ ormconfig.js
@@ -0,0 +1,13 @@
+require('dotenv/config')
+
+module.exports = {
+  type: 'mysql',
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  synchronize: false,
+  logging: true,
+  entities: ['src/entities/**/*.ts'],
+}
