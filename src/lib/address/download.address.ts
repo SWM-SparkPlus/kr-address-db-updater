c357642d730c5fd1a6ce856235c4d47c2959b022
Tay Park <interrupt@kakao.com>
2021-09-09 14:58:02 +0900

    refactor: download addresss to use target date only

--- src/lib/address/download.address.ts
+++ src/lib/address/download.address.ts
@@ -1,4 +1,3 @@
-import { EventEmitter } from 'events'
 import { createWriteStream, rmSync } from 'fs'
 import dayjs from 'dayjs'
 import { downloadFileAndGetEntries } from '../downloadAndGetEntries'
@@ -8,70 +7,50 @@ import { writeAddressFileAndImport } from './write.address'
 import { downloadPathHandler } from '../path/handler.path'
 import { TDownloadFileOption } from '../../types/option.type'
 
-const downloadOnlyEvent = new EventEmitter()
-downloadOnlyEvent.on('assigned', () => {})
-downloadOnlyEvent.on('finish', (target: string) => {
-  logger.info(`[DonwloadOnlyScriptCompletion] Download ${target} completed.`)
-})
-
-async function downloadAddressFilesOnly(downloadFlag: string) {
+export async function downloadAddressFilesOnly(targetDate: string) {
   downloadPathHandler()
 
-  const argCandidate = ['--daily', '-d', '--total', '-t']
-
-  if (argCandidate.includes(downloadFlag)) {
-    const date = new Date()
-    let url = ''
-    let downloadDir = ''
-
-    // 매개변수에 따라 다르게 처리
-    if (downloadFlag === '--daily' || downloadFlag === '-d') {
-      const yesterday = dayjs(date.setDate(date.getDate() - 1)).format('YYYYMMDD')
-      url = encodeURI(
-        `https://www.juso.go.kr/dn.do?reqType=DCM&stdde=${yesterday}&indutyCd=999&purpsCd=999&indutyRm=수집종료&purpsRm=수집종료`
-      )
-      downloadDir = dailyDir
-
-      logger.info(`[Preparation] Start on ${date}, download based on ${yesterday}`)
-    } else if (downloadFlag === '--total' || downloadFlag === '-t') {
-      const previousMonth = dayjs(date.setMonth(date.getMonth() - 1)).format('YYYYMM')
-      url = encodeURI(
-        `https://www.juso.go.kr/dn.do?reqType=ALLMTCHG&regYmd=${previousMonth.slice(
-          0,
-          4
-        )}&ctprvnCd=00&gubun=MTCH&stdde=${previousMonth}&fileName=${previousMonth}_주소DB_전체분.zip&realFileName=${previousMonth}ALLMTCHG00.zip&indutyCd=999&purpsCd=999&indutyRm=수집종료&purpsRm=수집종료`
-      )
-      downloadDir = totalDir
-      logger.info(`[Preparation] Start on ${date}, download based on ${previousMonth}`)
-    }
-
-    logger.info(`[DownloadScriptStart]`)
-
-    const writeStream = createWriteStream(`${downloadDir}/address_file_DB.zip`)
-    ;(await downloadFileAndGetEntries({ url, writeStream } as TDownloadFileOption)).forEach(
-      entry => {
-        entry.getDataAsync((data, err) => {
-          if (err) throw err
-
-          writeAddressFileAndImport({
-            data,
-            entryOfZip: entry,
-            writeDir: downloadDir,
-            doImport: false,
-          })
-        })
-      }
-    )
-
-    rmSync(writeStream.path)
-  } else {
-    logger.error(`[UnexpectedArgvError] argv ${arg} is unsupported.`)
-    process.exit(0)
+  if (!dayjs(targetDate).isValid()) {
+    logger.error(`[DownloadAddressError] Input date "${targetDate}" is invalid`)
+    return process.exit(1)
   }
-}
 
-const arg = process.argv[2]
-downloadAddressFilesOnly(arg).catch(err => {
-  logger.error(`[UnexpectedError] ${err}`)
-  process.exit(1)
-})
+  const downloadFlag = targetDate.length === 8 ? 'total' : 'daily'
+  let url = ''
+  let downloadDir = ''
+
+  if (downloadFlag === 'daily') {
+    url = encodeURI(
+      `https://www.juso.go.kr/dn.do?reqType=DCM&stdde=${targetDate}&indutyCd=999&purpsCd=999&indutyRm=수집종료&purpsRm=수집종료`
+    )
+    downloadDir = dailyDir
+  } else if (downloadFlag === 'total') {
+    url = encodeURI(
+      `https://www.juso.go.kr/dn.do?reqType=ALLMTCHG&regYmd=${targetDate.slice(
+        0,
+        4
+      )}&ctprvnCd=00&gubun=MTCH&stdde=${targetDate}&fileName=${targetDate}_주소DB_전체분.zip&realFileName=${targetDate}ALLMTCHG00.zip&indutyCd=999&purpsCd=999&indutyRm=수집종료&purpsRm=수집종료`
+    )
+    downloadDir = totalDir
+  }
+
+  logger.info(`[Preparation] Downloads start with ${targetDate}`)
+  logger.info(`[DownloadScriptStart]`)
+
+  const writeStream = createWriteStream(`${downloadDir}/address_file_DB.zip`)
+  ;(await downloadFileAndGetEntries({ url, writeStream } as TDownloadFileOption)).forEach(entry => {
+    entry.getDataAsync((data, err) => {
+      if (err) throw err
+
+      writeAddressFileAndImport({
+        data,
+        entryOfZip: entry,
+        writeDir: downloadDir,
+        doImport: false,
+      })
+    })
+  })
+
+  rmSync(writeStream.path)
+  logger.info(`[DownloadDailyAddressComplete] Download ${targetDate} completed.`)
+}
