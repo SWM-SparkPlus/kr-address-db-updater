ab93f90a3bc4f71d3e927d7c5e9118fce05bd9a5
Tay Park <interrupt@kakao.com>
2021-09-10 13:51:47 +0900

    add bupjungsidocode(법정시도코드) on type

--- src/types/sido.collections.ts
+++ src/types/sido.collections.ts
@@ -76,3 +76,25 @@ export enum EIndexTables {
   JibunAddress = 'jibun_manage_number_index',
   RoadnameAddress = 'road_manage_number_index',
 }
+
+type BupjungSidoCode = { [key in TSido]: number }
+
+export const BupjungSidoCodeMap: BupjungSidoCode = {
+  서울특별시: 11,
+  부산광역시: 26,
+  대구광역시: 27,
+  인천광역시: 28,
+  광주광역시: 29,
+  대전광역시: 30,
+  울산광역시: 31,
+  세종특별자치시: 36,
+  경기도: 41,
+  강원도: 42,
+  충청북도: 43,
+  충청남도: 44,
+  전라북도: 45,
+  전라남도: 46,
+  경상북도: 47,
+  경상남도: 48,
+  제주특별자치도: 50,
+}
