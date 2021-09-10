9b08a4815e50e779c18e10c0035cb3516f41367c
Tay Park <interrupt@kakao.com>
2021-09-10 14:20:47 +0900

    adding main.ts for command integration

--- src/lib/address/doImportFile.address.ts
+++ src/lib/address/doImportFile.address.ts
@@ -3,13 +3,15 @@ import { readdirSync } from 'fs'
 import { importToDb } from './importFile.address'
 import { logger } from '../logger'
 
-const files = readdirSync(path.resolve(__dirname) + '/../../../resources/total')
+export function doImportFilesToDb() {
+  const files = readdirSync(path.resolve(__dirname) + '/../../../resources/total')
 
-try {
-  files.map(fileName => {
-    importToDb(fileName.split('.')[0])
-  })
-} catch (err) {
-  logger.error(`[FileImportError] ${err}`)
-  process.exit(1)
+  try {
+    files.map(fileName => {
+      importToDb(fileName.split('.')[0])
+    })
+  } catch (err) {
+    logger.error(`[FileImportError] ${err}`)
+    process.exit(1)
+  }
 }
