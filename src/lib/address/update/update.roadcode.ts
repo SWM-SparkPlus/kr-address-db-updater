42d78ae1c1d7d10f0237578f84c6ad08915ec0cf
Tay Park <interrupt@kakao.com>
2021-09-10 16:55:32 +0900

    replace orm query to raw query

--- src/lib/address/update/update.roadcode.ts
+++ src/lib/address/update/update.roadcode.ts
@@ -1,6 +1,4 @@
 import { Connection } from 'typeorm'
-import { addMetadata } from '../../../typeorm/addMetadata'
-import { RoadcodeEntity } from '../../../typeorm/entities/roadcode.entity'
 
 /**
  * 도로명코드 테이블 업데이트 함수
@@ -12,43 +10,10 @@ export async function updateRoadcodeTable(connection: Connection, data: string) 
   if (data === 'No Data') return
 
   const splitData = data.split('|')
-  const inputData: RoadcodeEntity = {
-    roadname_code: splitData[0],
-    roadname: splitData[1],
-    roadname_eng: splitData[2],
-    eupmyeondong_serial_number: splitData[3],
-    sido: splitData[4],
-    sido_eng: splitData[5],
-    sigungu: splitData[6],
-    sigungu_eng: splitData[7],
-    eupmyeondong: splitData[8],
-    eupmyeondong_eng: splitData[9],
-    eupmyeondong_type: splitData[10],
-    eupmyeondong_code: splitData[11],
-    is_using: splitData[12],
-    change_reason: splitData[13],
-    change_history: splitData[14],
-    declare_date: splitData[15],
-    expire_date: splitData[16],
-  }
-
-  addMetadata(connection, RoadcodeEntity)
-
-  const findResult = await connection.manager.findOne(RoadcodeEntity, {
-    roadname_code: inputData.roadname_code,
-    eupmyeondong_serial_number: inputData.eupmyeondong_serial_number,
-  })
-
-  if (!findResult) {
-    connection.manager.save(RoadcodeEntity, inputData, { reload: false })
-  } else {
-    connection.manager.update(
-      RoadcodeEntity,
-      {
-        roadname_code: inputData.roadname_code,
-        eupmyeondong_code: inputData.eupmyeondong_code,
-      },
-      inputData
-    )
-  }
+  const [roadname_code, , , eupmyeondong_serial_number, , , , , , , , , , change_reason] = splitData
+  const sql =
+    change_reason === '63'
+      ? `DELETE FROM roadname_code WHERE roadname_code = '${roadname_code}' AND eupmyeondong_serial_number = '${eupmyeondong_serial_number}'`
+      : `REPLACE INTO roadname_code VALUES ('${splitData.join("','")}')`
+  connection.manager.query(sql)
 }
