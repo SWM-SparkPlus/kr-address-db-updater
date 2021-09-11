8e0fa2b5762119f5dd490a446938ca0a571d710c
Tay Park <interrupt@kakao.com>
2021-09-11 23:32:46 +0900

    update mysql settings
    - update my.cnf to enable error and mysqld logging
    - update ormLogger to get more detailed logs

--- src/typeorm/connection.ts
+++ src/typeorm/connection.ts
@@ -1,26 +1,7 @@
 import 'reflect-metadata'
-import 'dotenv/config'
-import {
-  Connection,
-  ConnectionManager,
-  ConnectionOptions,
-  createConnection,
-  getConnection,
-  getConnectionManager,
-} from 'typeorm'
+import { ConnectionManager } from 'typeorm'
 import TypeOrmLogger from './ormLogger'
 import { ormConfig } from './typeorm.config'
-
-// export async function getDbConnection(connectionOption: ConnectionOptions): Promise<Connection> {
-//   try {
-//     const defaultConnection = getConnectionManager().get()
-
-//     if (!defaultConnection) return await createConnection(Object.assign(connectionOption, { logger: new TypeOrmLogger() }))
-//     else return defaultConnection
-//   } catch (err) {
-//     throw err
-//   }
-// }
 
 let sequence = 0
 const connectionManager = new ConnectionManager()
