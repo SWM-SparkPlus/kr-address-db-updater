a122dbf47f18d82d3c9c33f48e727fd6acd53f0e
Tay Park <interrupt@kakao.com>
2021-09-13 14:31:10 +0900

    add update query on integrated table

--- src/lib/address/update/update.juso.ts
+++ src/lib/address/update/update.juso.ts
@@ -1,8 +1,8 @@
 import PoolConnection from 'mysql2/typings/mysql/lib/PoolConnection'
+import { RoadcodeEntity } from '../../../typeorm/entities/roadcode.entity'
 import { BupjungSidoCodeMap, SidoObject, TBupjungcode } from '../../../types/sido.collections'
 import { logger } from '../../logger'
 import { queryWithDbcp } from '../../mysqlConnection'
-import { zipcodeDecoder } from '../../zipcode/zipcode.decoder'
 
 /**
  * 도로명주소 테이블 업데이트 함수
@@ -27,16 +27,62 @@ export async function updateJusoTable(connection: PoolConnection, data: string) 
     previous_roadname_address,
     has_detail,
   ] = splitData
+  const sidoEngName = SidoObject[BupjungSidoCodeMap[manage_number.slice(0, 2) as TBupjungcode]]
+  const targetTable = `roadname_address_${sidoEngName}`
+  const targetIntegratedTable = `integrated_address_${sidoEngName}`
 
-  const sidoEngName = SidoObject[BupjungSidoCodeMap[manage_number?.slice(0, 2) as TBupjungcode]]
-
-  const sql =
-    change_reason_code === '63'
-      ? `DELETE FROM roadname_address_${sidoEngName} WHERE manage_number = '${manage_number}'`
-      : `REPLACE INTO roadname_address_${sidoEngName} VALUES ('${manage_number}', '${roadname_code}', '${eupmyeondong_serial_number}', '${is_basement}', '${building_primary_number}', '${building_secondary_number}', '${basic_state_number}', '${change_reason_code}', '${notice_date}', '${previous_roadname_address}', '${has_detail}')`
+  // const sql =
+  //   change_reason_code === '63'
+  //     ? `DELETE FROM roadname_address_${sidoEngName} WHERE manage_number = '${manage_number}'`
+  //     : `REPLACE INTO roadname_address_${sidoEngName} VALUES ('${manage_number}', '${roadname_code}', '${eupmyeondong_serial_number}', '${is_basement}', '${building_primary_number}', '${building_secondary_number}', '${basic_state_number}', '${change_reason_code}', '${notice_date}', '${previous_roadname_address}', '${has_detail}')`
 
   try {
-    await queryWithDbcp(connection, sql)
+    const findOneData = await queryWithDbcp(
+      connection,
+      `SELECT * FROM ${targetTable} WHERE manage_number = '${manage_number}' LIMIT 1`
+    )
+
+    if (!findOneData && change_reason_code === '31') {
+      queryWithDbcp(
+        connection,
+        `INSERT INTO ${targetTable} VALUES ('${manage_number}', '${roadname_code}', '${eupmyeondong_serial_number}', '${is_basement}', '${building_primary_number}', '${building_secondary_number}', '${basic_state_number}', '${change_reason_code}', '${notice_date}', '${previous_roadname_address}', '${has_detail}')`
+      )
+    } else if (findOneData && change_reason_code === '34') {
+      queryWithDbcp(
+        connection,
+        `UPDATE ${targetTable} SET roadname_code = '${roadname_code}', eupmyeondong_serial_number = '${eupmyeondong_serial_number}', is_basement = '${is_basement}', building_primary_number = '${building_primary_number}', building_secondary_number = '${building_secondary_number}', basic_state_number = '${basic_state_number}', change_reason_code = '${change_reason_code}', notice_date = '${notice_date}', previous_roadname_address = '${previous_roadname_address}', has_detail = '${has_detail}' WHERE manage_number = '${manage_number}'`
+      )
+    } else if (findOneData && change_reason_code === '34') {
+      queryWithDbcp(
+        connection,
+        `DELETE FROM ${targetTable} WHERE manage_number = '${manage_number}'`
+      )
+    }
+
+    const findData = await queryWithDbcp(
+      connection,
+      `SELECT * FROM ${targetIntegratedTable} WHERE manage_number = '${manage_number}' LIMIT 1`
+    )
+
+    if (!findData && change_reason_code === '31') {
+      await queryWithDbcp(
+        connection,
+        `INSERT INTO ${targetIntegratedTable} (manage_number, roadname_code, zipcode, is_basement, building_primary_number, building_secondary_number) VALUES ('${manage_number}', '${roadname_code}', '${basic_state_number}', '${is_basement}, '${building_primary_number}', '${building_secondary_number}')`
+      )
+      const roadcodeData: RoadcodeEntity = await queryWithDbcp(
+        connection,
+        `SELECT * FROM roadname_code WHERE roadname_code = '${roadname_code}' LIMIT 1`
+      )
+      await queryWithDbcp(
+        connection,
+        `UPDATE ${targetIntegratedTable} SET sido = '${roadcodeData.sido}', sigungu = '${roadcodeData.sigungu}', eupmyeondong = '${roadcodeData.eupmyeondong}', roadname = '${roadcodeData.roadname}' WHERE roadname_code = '${roadname_code}'`
+      )
+    } else if (findData && change_reason_code === '34') {
+      queryWithDbcp(
+        connection,
+        `UPDATE ${targetIntegratedTable} SET roadname_code = '${roadname_code}', zipcode = '${basic_state_number}', is_basement = '${is_basement}', building_primary_number = '${building_primary_number}', building_secondary_number = '${building_secondary_number}' WHERE manage_number = '${manage_number}'`
+      )
+    }
   } catch (err) {
     logger.error(`[UPDATE_JUSO_ERROR] ${err}`)
   }
