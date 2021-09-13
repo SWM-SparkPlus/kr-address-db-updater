7a6aca9ffaa52b5636decfeffd927ac4f88c7fa6
Tay Park <interrupt@kakao.com>
2021-09-13 14:31:10 +0900

    add update query on integrated table

--- src/lib/address/update/update.addinfo.ts
+++ src/lib/address/update/update.addinfo.ts
@@ -24,13 +24,35 @@ export async function updateAddinfoTable(connection: PoolConnection, data: strin
     changeReasonCode,
   ] = splitData
   const sidoEngName = SidoObject[BupjungSidoCodeMap[manage_number?.slice(0, 2) as TBupjungcode]]
-  const sql =
-    changeReasonCode === '63'
-      ? `DELETE FROM additional_info_${sidoEngName} WHERE manage_number = '${manage_number}'`
-      : `REPLACE INTO additional_info_${sidoEngName} VALUES ('${manage_number}', '${hangjungdong_code}', '${hangjungdong}', '${zipcode}', '${zipcode_serial_number}', '${bulk_delivery_building_name}', '${master_building_name}', '${sigungu_building_name}', '${is_apt}')`
+  const targetTable = `additional_info_${sidoEngName}`
+
+  // const sql =
+  //   changeReasonCode === '63'
+  //     ? `DELETE FROM additional_info_${sidoEngName} WHERE manage_number = '${manage_number}'`
+  //     : `REPLACE INTO additional_info_${sidoEngName} VALUES ('${manage_number}', '${hangjungdong_code}', '${hangjungdong}', '${zipcode}', '${zipcode_serial_number}', '${bulk_delivery_building_name}', '${master_building_name}', '${sigungu_building_name}', '${is_apt}')`
 
   try {
-    await queryWithDbcp(connection, sql)
+    const findOneData = await queryWithDbcp(
+      connection,
+      `SELECT * FROM ${targetTable} WHERE manage_number = '${manage_number}' LIMIT 1`
+    )
+
+    if (!findOneData && changeReasonCode === '31') {
+      queryWithDbcp(
+        connection,
+        `INSERT INTO ${targetTable} VALUES ('${manage_number}', '${hangjungdong_code}', '${hangjungdong}', '${zipcode}', '${zipcode_serial_number}', '${bulk_delivery_building_name}', '${master_building_name}', '${sigungu_building_name}', '${is_apt}')`
+      )
+    } else if (findOneData && changeReasonCode === '34') {
+      queryWithDbcp(
+        connection,
+        `UPDATE ${targetTable} SET hangjungdong_code = '${hangjungdong_code}', hangjungdong = '${hangjungdong}', zipcode = '${zipcode}', zipcode_serial_number = '${zipcode_serial_number}', bulk_delivery_building_name = '${bulk_delivery_building_name}', master_building_name = '${master_building_name}', sigungu_building_name = '${sigungu_building_name}', is_apt = '${is_apt}' WHERE manage_number = '${manage_number}'`
+      )
+    } else if (findOneData && changeReasonCode === '34') {
+      queryWithDbcp(
+        connection,
+        `DELETE FROM ${targetTable} WHERE manage_number = '${manage_number}'`
+      )
+    }
   } catch (err) {
     logger.error(`[UPDATE_ADDINFO_ERROR] ${err}`)
   }
--- src/lib/address/update/update.jibun.ts
+++ src/lib/address/update/update.jibun.ts
@@ -33,13 +33,54 @@ export async function updateJibunTable(connection: PoolConnection, data: string)
     change_reason_code,
   ] = splitData
   const sidoEngName = SidoObject[BupjungSidoCodeMap[manage_number.slice(0, 2) as TBupjungcode]]
-  const sql =
-    change_reason_code === '63'
-      ? `DELETE FROM jibun_address_${sidoEngName} WHERE manage_number = '${manage_number}' AND serial_number = '${serial_number}'`
-      : `REPLACE INTO jibun_address_${sidoEngName} VALUES ('${manage_number}', '${serial_number}', '${bupjungdong_code}', '${sido}', '${sigungu}', '${bupjungeupmyeondong}', '${bupjungli}', '${is_mountain}', '${jibun_primary_number}', '${jibun_secondary_number}', '${is_representation}')`
+  const targetTable = `jibun_address_${sidoEngName}`
+  const targetIntegratedTable = `integrated_address_${sidoEngName}`
+
+  // const sql =
+  // change_reason_code === '63'
+  // ? `DELETE FROM ${targetTable} WHERE manage_number = '${manage_number}' AND serial_number = '${serial_number}'`
+  // : `REPLACE INTO ${targetTable} VALUES ('${manage_number}', '${serial_number}', '${bupjungdong_code}', '${sido}', '${sigungu}', '${bupjungeupmyeondong}', '${bupjungli}', '${is_mountain}', '${jibun_primary_number}', '${jibun_secondary_number}', '${is_representation}')`
 
   try {
-    await queryWithDbcp(connection, sql)
+    const findOneData = await queryWithDbcp(
+      connection,
+      `SELECT * FROM ${targetTable} WHERE manage_number = '${manage_number}' AND serial_number = '${serial_number}' LIMIT 1`
+    )
+
+    if (!findOneData && change_reason_code === '31') {
+      queryWithDbcp(
+        connection,
+        `INSERT INTO ${targetTable} VALUES ('${manage_number}', '${serial_number}', '${bupjungdong_code}', '${sido}', '${sigungu}', '${bupjungeupmyeondong}', '${bupjungli}', '${is_mountain}', '${jibun_primary_number}', '${jibun_secondary_number}', '${is_representation}')`
+      )
+    } else if (findOneData && change_reason_code === '34') {
+      queryWithDbcp(
+        connection,
+        `UPDATE ${targetTable} SET serial_number = '${serial_number}', bupjungdong_code = '${bupjungdong_code}', sido = '${sido}', sigungu = '${sigungu}', bupjungeupmyeondong = '${bupjungeupmyeondong}', bupjungli = '${bupjungli}', is_mountain = '${is_mountain}', jibun_primary_number = '${jibun_primary_number}', jibun_secondary_number = '${jibun_secondary_number}', is_representation = '${is_representation}' WHERE manage_number = '${manage_number}' AND serial_number = '${serial_number}'`
+      )
+    } else if (findOneData && change_reason_code === '34') {
+      queryWithDbcp(
+        connection,
+        `DELETE FROM ${targetTable} WHERE manage_number = '${manage_number}' AND serial_number = '${serial_number}'`
+      )
+    }
+
+    // queryWithDbcp(connection, sql)
+    const findData = await queryWithDbcp(
+      connection,
+      `SELECT * FROM ${targetIntegratedTable} WHERE manage_number = '${manage_number}' LIMIT 1`
+    )
+
+    if (!findData && change_reason_code === '31') {
+      queryWithDbcp(
+        connection,
+        `INSERT INTO ${targetIntegratedTable} (manage_number, bupjungli, bupjungdong_code) VALUES ('${manage_number}', '${bupjungli}', '${bupjungdong_code}')`
+      )
+    } else if (findData && change_reason_code === '34') {
+      queryWithDbcp(
+        connection,
+        `UPDATE ${targetIntegratedTable} SET bupjungli = '${bupjungli}', bupjungdong_code = '${bupjungdong_code}' WHERE manage_number = '${manage_number}'`
+      )
+    }
   } catch (err) {
     logger.error(`[UPDATE_JUSO_ERROR] ${err}`)
   }
